0164aaa0ad7870f628d3b9df16131fcd0aa69bcc
i-ankit-here <closetooankit@gmail.com>
2024-12-15 18:08:34 +0530

    inventory product classes updated to support bills

--- product.cpp
+++ product.cpp
@@ -1,9 +1,13 @@
 #include "iostream"
 #include "iomanip"
+#include "string.h"
 using namespace std;
 class item{
+    friend class inventory;
+    friend class bill;
 protected:
     int itemid;
+    char type[30];
     char name[50];
     float price;
     int quantity;
@@ -11,17 +15,40 @@ protected:
     float tax;
 public:
     item(){
-        setName();
-        setPrice();
-        setQuantity();
+        try{
+            setName();
+            setId();
+            setPrice();
+            setQuantity();
+        }catch(char const str[]){
+            cout<<"str"<<endl;
+        }
+        catch(...){
+            cout<<"Some error occured while creating item"<<endl;
+        }
     };
+    item(int a){};
+    item(char str[],float prc,float qty){
+        strcpy(name,str);
+        price = prc;
+        quantity = qty;
+    }
+    ~item(){};
+    
     void setName(){
         cout<<"Enter name of item: ";
         cin>>name;
+        if(!name || !name[0])throw("No item name provided!");
     };
+    void setId(){
+        cout<<"Enter productId: ";
+        cin>>itemid;
+        if(!itemid)throw("ItemId is required");
+    }
     void setPrice(){
         cout<<"Enter price of item: ";
         cin>>price;
+        if(price==0)throw("Zero price is set");
     };
     void setQuantity(){
         cout<<"Enter quantity of item: ";
@@ -31,26 +58,32 @@ public:
     virtual void display() = 0;
     virtual float calculate_discount() = 0;
     virtual float calculate_tax() = 0;
-
+    virtual float getDiscount_percent() = 0;
+    virtual float getTax_percent() = 0;
 };
 
 class electronic:public item{
     float static tax_percent;
     float static discount_percent;
+    friend class inventory;
 public:
     electronic():item(){
         tax = calculate_tax();
         discount = calculate_discount();
+        strcpy(type,"electronic");
     }
+    electronic(int a):item(a){};
     void display(){
-        cout<<"------------------------------------------------------------"<<endl;
+        cout<<"============================================================="<<endl;
         cout<<setw(25)<<"Name of item: "<<name<<endl;
+        cout<<setw(25)<<"Product Id: "<<itemid<<endl;
+        cout<<setw(25)<<"Product Type: "<<type<<endl;
         cout<<setw(25)<<"Price of item: "<<price<<endl;
         cout<<setw(25)<<"Quantity of item: "<<quantity<<endl;
         cout<<setw(25)<<"Tax(%): "<<tax_percent<<endl;
         cout<<setw(25)<<"Discout(%): "<<discount_percent<<endl;
         cout<<setw(25)<<"Final Price of item: "<<price+tax-discount<<endl;
-        cout<<"------------------------------------------------------------"<<endl;
+        cout<<"============================================================="<<endl;
     }
     float calculate_discount(){
         return price*discount_percent/(float)100;
@@ -58,6 +91,12 @@ public:
     float calculate_tax(){
         return price*tax_percent/(float)100;
     }
+    virtual float getDiscount_percent(){
+        return this->discount_percent;
+    }
+    virtual float getTax_percent(){
+        return this->tax_percent;
+    };
 };
 
 float electronic::tax_percent = 12;
@@ -66,18 +105,25 @@ float electronic::discount_percent = 10;
 class grocery:public item{
     float static tax_percent;
     float static discount_percent;
+    friend class inventory;
 public:
     grocery():item(){
         tax = calculate_tax();
         discount = calculate_discount();
+        strcpy(type,"grocery");
     }
+    grocery(int a):item(a){};
     void display(){
-        cout<<"Name of item: "<<name<<setw(40)<<endl;
-        cout<<"Price of item: "<<price<<endl;
-        cout<<"Quantity of item: "<<quantity<<endl;
-        cout<<"Tax(%): "<<tax_percent<<endl;
-        cout<<"Discout(%): "<<discount_percent<<endl;
-        cout<<"Final Price of item: "<<price+tax-discount<<endl;
+        cout<<"============================================================="<<endl;
+        cout<<setw(25)<<"Name of item: "<<name<<endl;
+        cout<<setw(25)<<"Product Id: "<<itemid<<endl;
+        cout<<setw(25)<<"Product Type: "<<type<<endl;
+        cout<<setw(25)<<"Price of item: "<<price<<endl;
+        cout<<setw(25)<<"Quantity of item: "<<quantity<<endl;
+        cout<<setw(25)<<"Tax(%): "<<tax_percent<<endl;
+        cout<<setw(25)<<"Discout(%): "<<discount_percent<<endl;
+        cout<<setw(25)<<"Final Price of item: "<<price+tax-discount<<endl;
+        cout<<"============================================================="<<endl;
     }
     float calculate_discount(){
         return price*discount_percent/(float)100;
@@ -85,6 +131,12 @@ public:
     float calculate_tax(){
         return price*tax_percent/(float)100;
     }
+    virtual float getDiscount_percent(){
+        return this->discount_percent;
+    }
+    virtual float getTax_percent(){
+        return this->tax_percent;
+    };
 };
 
 float grocery::tax_percent = 12;
